c6c3a1792b3b80b3040d9d85bffa6e1f97fb4268
Shadofer <shadowrlrs@gmail.com>
2022-04-25 16:34:07 +0300

    Add custom message setting.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,22 @@
+import { SettingsSection } from 'spcr-settings';
+
 async function main() {
   while (!Spicetify?.showNotification) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
   
-  // Show welcome message on start.
-  Spicetify.showNotification(`Welcome back to Spotify!`);
+  // Setup settings section
+  const settings = new SettingsSection('Welcomer', 'welcomer-settings');
+
+  const welcomerMsg = localStorage.getItem('welcomer-msg') || 'Welcome back to Spotify!';
+
+  settings.addInput('welcomer-msg', 'Custom welcome message', welcomerMsg, (() => {
+    localStorage.setItem('welcomer-msg', settings.getFieldValue('welcomer-msg'));
+  }));
+
+  settings.pushSettings();
+
+  Spicetify.showNotification(welcomerMsg);
 }
 
 export default main;
